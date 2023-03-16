60f4929e271672b48b281b86b460a4618f7abeb9
yurbass <yurbass1@rambler.ru>
2023-03-16 20:19:02 +0300

    added routing for employees component

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.scss']
+  styleUrls: ['./employees.component.scss'],
 })
 export class EmployeesComponent {
+  title: string;
 
+  
+  constructor() {
+    this.title = 'Employees component';
+  }
 }
