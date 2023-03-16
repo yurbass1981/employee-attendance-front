2e951bfa59537bbb3ecd690e5c7675addb408c51
yurbass <yurbass1@rambler.ru>
2023-03-16 20:45:01 +0300

    Added employeeService

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmployyesService } from '../services/employyes.service';
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
   styleUrls: ['./employees.component.scss'],
 })
-export class EmployeesComponent {
+export class EmployeesComponent implements OnInit {
   title: string;
+  employeesService: EmployyesService;
 
-  
-  constructor() {
+  constructor(employeesService: EmployyesService) {
     this.title = 'Employees component';
+    this.employeesService = employeesService;
   }
+  
+  ngOnInit(): void {}
 }
