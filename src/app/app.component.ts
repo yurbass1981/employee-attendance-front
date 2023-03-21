2363e9d6685ab475accf55140441b18be8045594
yurbass <yurbass1@rambler.ru>
2023-03-21 18:05:54 +0300

    injected EmployeesService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { EmployeesService } from './services/employees.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  
+  constructor(private employeesService: EmployeesService) {}
 }
