368c0b593785283a439c2d939a2b7fc55e3eb443
Yuri Ilyushin <yurbass1@rambler.ru>
2023-03-31 13:25:08 +0300

    added getEmployees method

--- src/app/services/employees.service.ts
+++ src/app/services/employees.service.ts
@@ -1,8 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { Employee } from '../models/employee-model';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeesService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+
+  getEmployees() {
+    return this.http.get<Employee>('http://localhost:5055/api/Employee');
+  }
 }
